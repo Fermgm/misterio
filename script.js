a8f3b486fe0d7d44c48ae020ed3fee3eb601d636
Fermgm <159826296+Fermgm@users.noreply.github.com>
2024-02-12 17:19:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.getElementById('siBtn').addEventListener('click', function() {
 
     // Mostrar el mensaje específico
     document.getElementById('messageContainer').style.display = 'block';
-    document.getElementById('messageContainer').innerHTML = '¡Siii! jejeje Te amoo BB <3';
+    document.getElementById('messageContainer').innerHTML = '¡Siii! jejeje Te amoo BB ♡';
 
     // Mostrar otro gif después de 3 segundos
     setTimeout(function() {
